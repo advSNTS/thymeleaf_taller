a2a67628f6cb790c04fe143829f9088cdde9a391
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-02-04 20:21:43 -0500

    Controller de las paginas adicionales

---
+++ src/main/java/co/javeriana/dw/thymeleaf/controller/WikiController.java
@@ -0,0 +1,30 @@
+package co.javeriana.dw.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WikiController {
+    
+    //Controller para todo menos para contacto
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/guia")
+    public String guia() {
+        return "guia/guia";
+    }
+
+    @GetMapping("/glosario")
+    public String glosario() {
+        return "glosario/glosario";
+    }
+
+    @GetMapping("/integrantes")
+    public String integrantes() {
+        return "integrantes/integrantes";
+    }
+}
